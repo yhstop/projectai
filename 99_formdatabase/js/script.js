7924d8a6b64088d8a434245e45638dd31da409c3
yhstop <62218972+yhstop@users.noreply.github.com>
2026-08-12 09:30:27 +0900

    Add files via upload

--- 99_formdatabase/js/script.js
+++ 99_formdatabase/js/script.js
@@ -22,5 +22,31 @@ form.addEventListener("submit", (e) => {
     })
     .catch((error) => {
       console.error("Error!", error.message);
+      msg.innerHTML = "전송 중 오류가 발생했습니다.";
+    });
+});";
+
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
+  })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+
+      setTimeout(() => {
+        msg.innerHTML = "";
+      }, 5000);
+
+      form.reset();
+    })
+    .catch((error) => {
+      console.error("Error!", error.message);
+      msg.innerHTML = "전송 중 오류가 발생했습니다.";
     });
 });
